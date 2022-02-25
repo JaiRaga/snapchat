8d7446fa48174bbe04c4210fb1db14a5324a6cc3
JaiRaga <raga.j.santhosh@gmail.com>
2022-02-25 15:00:08 +0530

    :sparkles: displays video feed from web camera

---
+++ src/components/camera/WebcamCapture.js
@@ -0,0 +1,32 @@
+import React, { useCallback, useRef, useState } from 'react'
+import Webcam from 'react-webcam'
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked'
+
+const videoConstraints = {
+	width: 250,
+	height: 400,
+	facingMode: 'user',
+}
+
+function WebcamCapture() {
+	const webcamRef = useRef(null)
+
+	const capture = useCallback(() => {
+		const imageSrc = webcamRef.current.getScreenshot()
+	}, [webcamRef])
+	return (
+		<div className='webcamCapture'>
+			<Webcam
+				audio={false}
+				height={videoConstraints.height}
+				ref={webcamRef}
+				screenshotFormat='image/jpeg'
+				width={videoConstraints.width}
+				videoConstraints={videoConstraints}
+			/>
+			<RadioButtonUncheckedIcon className="webcamCapture__button" onClick={capture} />
+		</div>
+	)
+}
+
+export default WebcamCapture
